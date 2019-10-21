58852114ea99271ac472d874eb6749a57075a02c
Larry-Osakwe <larryosak@gmail.com>
2019-10-21 16:28:35 -0700

    create boat post route

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const datastore = new Datastore();
 
-const LODGING = "Lodging";
+const BOAT = "Boat";
 
 const router = express.Router();
 
@@ -17,28 +17,28 @@ function fromDatastore(item){
     return item;
 }
 
-/* ------------- Begin Lodging Model Functions ------------- */
-function post_lodging(name, description, price){
-    var key = datastore.key(LODGING);
-	const new_lodging = {"name": name, "description": description, "price": price};
-	return datastore.save({"key":key, "data":new_lodging}).then(() => {return key});
+/* ------------- Begin Boat Model Functions ------------- */
+function post_boat(name, type, length){
+    var key = datastore.key(BOAT);
+	const new_boat = {"name": name, "type": type, "length": length};
+	return datastore.save({"key":key, "data":new_boat}).then(() => {return key});
 }
 
-function get_lodgings(){
-	const q = datastore.createQuery(LODGING);
+function get_boats(){
+	const q = datastore.createQuery(BOAT);
 	return datastore.runQuery(q).then( (entities) => {
 			return entities[0].map(fromDatastore);
 		});
 }
 
-function put_lodging(id, name, description, price){
-    const key = datastore.key([LODGING, parseInt(id,10)]);
-    const lodging = {"name": name, "description": description, "price": price};
-    return datastore.save({"key":key, "data":lodging});
+function put_boat(id, name, description, price){
+    const key = datastore.key([BOAT, parseInt(id,10)]);
+    const boat = {"name": name, "description": description, "price": price};
+    return datastore.save({"key":key, "data":boat});
 }
 
-function delete_lodging(id){
-    const key = datastore.key([LODGING, parseInt(id,10)]);
+function delete_boat(id){
+    const key = datastore.key([BOAT, parseInt(id,10)]);
     return datastore.delete(key);
 }
 
@@ -47,29 +47,29 @@ function delete_lodging(id){
 /* ------------- Begin Controller Functions ------------- */
 
 router.get('/', function(req, res){
-    const lodgings = get_lodgings()
-	.then( (lodgings) => {
-        res.status(200).json(lodgings);
+    const boats = get_boats()
+	.then( (boats) => {
+        res.status(200).json(boats);
     });
 });
 
 router.post('/', function(req, res){
-    post_lodging(req.body.name, req.body.description, req.body.price)
-    .then( key => {res.status(200).send('{ "id": ' + key.id + ' }')} );
+    post_boat(req.body.name, req.body.type, req.body.length)
+    .then( key => {res.status(201).send('{ "id": ' + key.id + ', "name": ' + req.body.name + ', "type": ' + req.body.type + ', "length": ' + req.body.length + ' }')} );
 });
 
 router.put('/:id', function(req, res){
-    put_lodging(req.params.id, req.body.name, req.body.description, req.body.price)
+    put_boat(req.params.id, req.body.name, req.body.type, req.body.length)
     .then(res.status(200).end());
 });
 
 router.delete('/:id', function(req, res){
-    delete_lodging(req.params.id).then(res.status(200).end())
+    delete_boat(req.params.id).then(res.status(200).end())
 });
 
 /* ------------- End Controller Functions ------------- */
 
-app.use('/lodgings', router);
+app.use('/boats', router);
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
